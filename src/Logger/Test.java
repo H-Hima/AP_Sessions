0142ab7cbeb2253e81f22cd3d403e8295f197f8d
Hossein Hima <mh.hima@gmail.com>
2019-02-25 11:34:46 +0330

    Add another log to the program

--- src/Logger/Test.java
+++ src/Logger/Test.java
@@ -7,6 +7,7 @@ public class Test {
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
 		Logger.getLogger().error("This is a fake error");
+		Logger.getLogger().info("This is a real information");
 	}
 
 }
