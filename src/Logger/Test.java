62e6f2e0318202a520e1423fdc3ee17bbd6c4b1c
Hossein Hima <mh.hima@gmail.com>
2019-02-25 11:56:44 +0330

    add a debug log to project

--- src/Logger/Test.java
+++ src/Logger/Test.java
@@ -7,6 +7,7 @@ public class Test {
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
 		Logger.getLogger().error("This is a fake error");
+		Logger.getLogger().debug("This is a verbos data");
 	}
 
 }
